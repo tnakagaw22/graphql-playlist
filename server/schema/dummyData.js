f2ae6cfb62d296fa9cef23316731624a99f5acd3
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2019-05-05 10:55:32 -0400

    added schema and return dummy data

---
+++ server/schema/dummyData.js
@@ -0,0 +1,9 @@
+const books = [
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1'},
+    { name: 'The Final Empire', genre: 'Fantasy', id: '2'},
+    { name: 'The Long Earch', genre: 'Sci-Fi', id: '3'}
+];
+
+module.exports = {
+    books
+}
---
+++ server/schema/schema.js
@@ -0,0 +1,31 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+const dummyData = require('./dummyData');
+const { GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    fields: () => ({
+        id: { type: GraphQLString},
+        name: {type: GraphQLString},
+        genre: {type: GraphQLString}
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: {id: {type: GraphQLString}},
+            resolve(parent, args) {
+                // code t oget data from db/ other source
+                return _.find(dummyData.books, {id: args.id});
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
