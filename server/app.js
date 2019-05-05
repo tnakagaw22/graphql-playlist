ab131aab4a88776e7294d1161ae737ff7d518676
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2019-05-05 14:40:44 -0400

    Added mongoose connection with config key setting

--- server/app.js
+++ server/app.js
@@ -1,14 +1,27 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
 const app = express();
+
+mongoose.connect(keys.mongoURI, {
+    auth: {
+        user: keys.mongoUserName,
+        password: keys.mongoPassword
+    }
+});
+
+mongoose.connection.once('open', () => {
+    console.log('connected to db');
+});
 
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }));
 
-app.listen(4000, () => {
-    console.log('now listing for requests on port 4000');
+app.listen(keys.port, () => {
+    console.log(`now listing for requests on port ${keys.port}`);
 });
