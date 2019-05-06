1ba17c9e41aae3f1006b3d2b12673a6b1498121b
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2019-05-05 21:56:25 -0400

    Connected react to graphql to load book list

--- server/app.js
+++ server/app.js
@@ -2,9 +2,12 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const keys = require('./config/keys');
 
 const app = express();
+
+app.use(cors());
 
 mongoose.connect(keys.mongoURI, {
     auth: {
