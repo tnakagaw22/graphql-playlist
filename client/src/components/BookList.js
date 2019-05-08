1cb37a5e8a1b127ee85519b83181468b45519fe7
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2019-05-08 07:41:57 -0400

    Added BookDetails

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -2,7 +2,17 @@ import React, { Component } from 'react';
 import {graphql} from 'react-apollo';
 import {getBooksQuery} from '../queries/queries';
 
+// components
+import BookDetails from './BookDetails';
+
 class BookList extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      selected: null
+    }
+  }
+
   displayBooks(){
     var data = this.props.data;
     if (data.loading){
@@ -10,7 +20,7 @@ class BookList extends Component {
     } else {
       return data.books.map(book => {
         return(
-          <li key={book.id}>{book.name}</li>
+          <li key={book.id} onClick={(e) => this.setState({selected: book.id})}>{book.name}</li>
         );
       })
     }
@@ -21,6 +31,8 @@ class BookList extends Component {
         <ul id="book-list">
             {this.displayBooks()}
         </ul>
+
+        <BookDetails bookId={this.state.selected}/>
       </div>
     );
   }
